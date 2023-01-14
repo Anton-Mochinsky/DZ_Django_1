e74fa1f3a89dd0418793586f74f39886e9da8407
Anton Mochinsky <avm199615@gmail.com>
2023-01-14 16:57:08 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    path = os.getcwd()
+    list_files = os.listdir(path)
+    return HttpResponse("**".join(list_files))
